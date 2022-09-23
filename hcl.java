6dcbdf138b3543ba2a9d3cf0d119c41a197d254a
parth-140 <111503939+parth-140@users.noreply.github.com>
2022-09-23 15:58:47 +0530

    Update hcl.java
    update

--- hcl.java
+++ hcl.java
@@ -1,5 +1,5 @@
 public class Main{
                 public static void main(String[]args){
-                System.out.println("deveops java 6");
+                System.out.println("hello java");
                 }
             }
